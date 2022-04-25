7cfed653a2c237f607b4dab19e0e08ad1c54de5a
leishaozaici <775460163@qq.com>
2022-04-25 17:06:22 +0800

    可变数组实现双端队列

--- Project_2/Deque.java
+++ Project_2/Deque.java
@@ -4,23 +4,23 @@
  *  Description:
  **************************************************************************** */
 
+import edu.princeton.cs.algs4.StdOut;
+
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
 public class Deque<Item> implements Iterable<Item> {
-    private Node first;
-    private Node last;
+    private Item[] a;
+    private int head;
+    private int tail;
     private int N;
-
-    private class Node() {
-        Item item;
-        Node next;
-    }
 
     // construct an empty deque
     public Deque() {
-
-
+        N = 0;
+        head = 0;
+        tail = 0;
+        a = (Item[]) new Object[1];
     }
 
     // is the deque empty?
@@ -35,70 +35,115 @@ public class Deque<Item> implements Iterable<Item> {
 
     // add the item to the front
     public void addFirst(Item item) {
-        Node oldfirst = first;
-        first = new Node();
-        first.item = item;
-        first.next = oldfirst;
-        if (isEmpty()) {
-            first = last;
+        if (item == null) {
+            throw new IllegalArgumentException();
         }
-        else {
-            first.next = oldfirst;
+        if (N == a.length) {
+            resize(a.length * 2);
         }
+        a[head--] = item;
         N++;
-
     }
 
     // add the item to the back
     public void addLast(Item item) {
-        Node oldlast = last;
-        last = new Node();
-        last.item = item;
-        last.next = null;
-        if (isEmpty()) {
-            first = last;
+        if (item == null) {
+            throw new IllegalArgumentException();
         }
-        else {
-            oldlast.next = last;
+        if (N == a.length) {
+            resize(a.length * 2);
         }
+        a[tail++] = item;
         N++;
     }
 
     // remove and return the item from the front
     public Item removeFirst() {
-        Item item = first.item;
-        first = first.next;
         if (isEmpty()) {
-            last = null;
-            N--;
+            throw new NoSuchElementException();
+        }
+        Item item = a[head];
+        a[head++] = null;
+        if (N > 0 && N == a.length / 4) {
+            resize(a.length / 2);
         }
         return item;
     }
 
     // remove and return the item from the back
-    public Item removeLast() throws NoSuchElementException {
-        Item item = last.item;
-
+    public Item removeLast() {
+        if (isEmpty()) {
+            throw new NoSuchElementException();
+        }
+        Item item = a[tail];
+        a[tail--] = null;
+        if (N > 0 && N == a.length / 4) {
+            resize(a.length / 2);
+        }
         return item;
     }
 
     // return an iterator over items in order from front to back
     public Iterator<Item> iterator() {
-        return new ListIterator();
+        return new DequeIterator();
     }
 
-    private class ListIterator implements Iterator<Item> {
+    private class DequeIterator implements Iterator<Item> {
+        private int i = N;
 
         public boolean hasNext() {
-            return N == 0;
+            return i > 0;
         }
 
         public Item next() {
-            return null;
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            return a[--i];
         }
     }
 
+    private void resize(int max) {
+        Item[] temp = (Item[]) new Object[max];
+        for (int i = 0; i < N; i++) {
+            temp[i] = a[i];
+        }
+        a = temp;
+    }
+
     // unit testing (required)
-    public static void main(String[] args)
+    public static void main(String[] args) {
+        Deque<String> deque = new Deque<>();
+        for (int i = 0; i < 3; i++) {
+            deque.addLast(i + "");
+        }
+        while (!deque.isEmpty()) {
+            StdOut.println(deque.removeFirst());
+        }
+        Deque<Integer> ideque = new Deque<>();
+        for (int i = 0; i < 20; i++) {
+            ideque.addFirst(i);
+        }
+        Iterator<Integer> iterator = ideque.iterator();
+        while (iterator.hasNext()) {
+            StdOut.println(iterator.next());
+        }
+
+        while (!ideque.isEmpty()) {
+            StdOut.println(ideque.removeLast());
+        }
+        System.out.println("************************");
+        for (int i = 0; i < 3; i++) {
+            if (i % 2 == 0) {
+                ideque.addFirst(i);
+            }
+            else {
+                StdOut.println(ideque.removeLast());
+            }
+        }
+
+    }
 
 }
+
+
