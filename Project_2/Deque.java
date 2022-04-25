49cc4790caccadf22eee35c0211f6dcc6839fadb
leishaozaici <775460163@qq.com>
2022-04-25 14:44:37 +0800

    修改

--- Project_2/Deque.java
+++ Project_2/Deque.java
@@ -5,43 +5,97 @@
  **************************************************************************** */
 
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 public class Deque<Item> implements Iterable<Item> {
+    private Node first;
+    private Node last;
+    private int N;
+
+    private class Node() {
+        Item item;
+        Node next;
+    }
 
     // construct an empty deque
-    public Deque()
+    public Deque() {
+
+
+    }
 
     // is the deque empty?
-    public boolean isEmpty()
+    public boolean isEmpty() {
+        return N == 0;
+    }
 
     // return the number of items on the deque
     public int size() {
-
+        return N;
     }
 
     // add the item to the front
     public void addFirst(Item item) {
+        Node oldfirst = first;
+        first = new Node();
+        first.item = item;
+        first.next = oldfirst;
+        if (isEmpty()) {
+            first = last;
+        }
+        else {
+            first.next = oldfirst;
+        }
+        N++;
 
     }
 
     // add the item to the back
     public void addLast(Item item) {
-
+        Node oldlast = last;
+        last = new Node();
+        last.item = item;
+        last.next = null;
+        if (isEmpty()) {
+            first = last;
+        }
+        else {
+            oldlast.next = last;
+        }
+        N++;
     }
 
     // remove and return the item from the front
     public Item removeFirst() {
-        return Item;
+        Item item = first.item;
+        first = first.next;
+        if (isEmpty()) {
+            last = null;
+            N--;
+        }
+        return item;
     }
 
     // remove and return the item from the back
-    public Item removeLast() {
-        return Item;
+    public Item removeLast() throws NoSuchElementException {
+        Item item = last.item;
+
+        return item;
     }
 
     // return an iterator over items in order from front to back
     public Iterator<Item> iterator() {
-        return
+        return new ListIterator();
+    }
+
+    private class ListIterator implements Iterator<Item> {
+
+        public boolean hasNext() {
+            return N == 0;
+        }
+
+        public Item next() {
+            return null;
+        }
     }
 
     // unit testing (required)
